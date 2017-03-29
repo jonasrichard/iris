6448d7c9288f88e67095780c37ceaedfe958e7d6
Richard Jonas <richard.jonas@wallapop.com>
2017-03-29 12:15:59 +0200

    Handling hooks

--- lib/hook.ex
+++ lib/hook.ex
@@ -2,23 +2,73 @@ defmodule Iris.Hook do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, [], [name: __MODULE__])
+  end
+
+  def add(hook, module, fun, prio) do
+    GenServer.call(__MODULE__, {:add, hook, module, fun, prio})
+  end
+
+  def delete(hook, module, fun, prio) do
+    GenServer.call(__MODULE__, {:delete, hook, module, fun, prio})
+  end
+
+  def run(hook, args) do
+    case :ets.lookup(:hooks, hook) do
+      [] ->
+        :ok
+      [{_, callbacks}] ->
+        run_callbacks(callbacks, args)
+    end
   end
 
   def init(_) do
     tab = :ets.new(:hooks, [:public, :named_table])
-    {:init, %{tab: tab}}
+    {:ok, %{tab: tab}}
   end
 
   def handle_call({:add, hook, module, fun, prio}, _from, state) do
     case :ets.lookup(:hooks, hook) do
       [{^hook, callbacks}] ->
-        new_hook = sort_by_prio([{module, fun, prio} | callbacks])
-        :ets.insert(:hooks, new_hook)
+        new_cbs = sort_by_prio([{module, fun, prio} | callbacks])
+        :ets.insert(:hooks, {hook, new_cbs})
       [] ->
         :ets.insert(:hooks, {hook, [{module, fun, prio}]})
     end
     {:reply, :ok, state}
+  end
+  def handle_call({:delete, hook, module, fun, prio}, _from, state) do
+    case :ets.lookup(:hooks, hook) do
+      [] ->
+        :ok
+      [{^hook, callbacks}] ->
+        new_cbs = List.delete(callbacks, {module, fun, prio})
+        :ets.insert(:hooks, {hook, new_cbs})
+    end
+    {:reply, :ok, state}
+  end
+
+  defp run_callbacks([], args) do
+    args
+  end
+  defp run_callbacks([{module, fun, _prio} | rest], args) do
+    try do
+      :erlang.apply(module, fun, args)
+    catch
+      ex, desc ->
+        {:error, {ex, desc}}
+    else
+      :ok ->
+        run_callbacks(rest, args)
+      {:ok, new_args} ->
+        run_callbacks(rest, new_args)
+      :stop ->
+        :ok
+      {:stop, result} ->
+        {:ok, result}
+      {:error, _reason} = error ->
+        error
+    end
   end
 
   defp sort_by_prio(callbacks) do
---
+++ test/hook_test.exs
@@ -0,0 +1,28 @@
+defmodule Iris.HookTest do
+  use ExUnit.Case
+
+  test "add/delete hook works" do
+    :dbg.tracer
+    :dbg.tpl(Iris.Hook, [])
+    :dbg.p(:all, :c)
+
+    {:ok, _pid} = Iris.Hook.start_link
+
+    :ok = Iris.Hook.add(:test_hook, __MODULE__, :first, 10)
+    assert Iris.Hook.run(:test_hook, [0]) == [1]
+
+    :ok = Iris.Hook.add(:test_hook, __MODULE__, :second, 20)
+    assert Iris.Hook.run(:test_hook, [0]) == [3]
+
+    :ok = Iris.Hook.delete(:test_hook, __MODULE__, :first, 10)
+    assert Iris.Hook.run(:test_hook, [0]) == [2]
+  end
+
+  def first(n) do
+    {:ok, [n + 1]}
+  end
+
+  def second(n) do
+    {:ok, [n + 2]}
+  end
+end
