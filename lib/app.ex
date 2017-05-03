2b5579e050dd2d4367dc5f53c91571bfc5df5e09
Richard Jonas <richard.jonas@erlang-solutions.com>
2017-05-03 08:22:19 +0200

    Add restart options

--- lib/app.ex
+++ lib/app.ex
@@ -60,7 +60,7 @@ defmodule Iris.ClientSup do
 
   def init(_) do
     children = [
-      worker(Iris.Client, [])
+      worker(Iris.Client, [restart: temporary])
     ]
     supervise(children, strategy: :simple_one_for_one)
   end
@@ -82,7 +82,7 @@ defmodule Iris.ChannelSup do
 
   def init(_) do
     children = [
-      worker(Iris.Channel, [])
+      worker(Iris.Channel, [restart: temporary])
     ]
     supervise(children, strategy: :simple_one_for_one)
   end
