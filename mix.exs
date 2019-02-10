40d4927a4e454e7150a61b766b0905ab6f61545d
Richard Jonas <richard.jonas@erlang-solutions.com>
2019-02-10 17:01:20 +0100

    Add Elixir release

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Iris.Mixfile do
 
   def project do
     [app: :iris,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+     version: "0.2.0",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: ExCoveralls],
@@ -18,13 +18,12 @@ defmodule Iris.Mixfile do
   end
 
   defp deps do
-    [#{:cowboy, github: "ninenines/cowboy", tag: "2.0.0-pre.7"},
-     {:credo, "~> 0.7", only: [:test, :dev]},
-     #{:dogma, "~> 0.1.8", only: :dev},
+    [{:credo, "~> 0.7", only: [:test, :dev]},
+     {:distillery, "~> 2.0"},
      {:excoveralls, "~> 0.7.1", only: :test},
-     {:httpoison, "~> 1.3.1", only: :test},
+     {:httpoison, "~> 1.5.0", only: :test},
      {:instream, "~> 0.18"},
-     {:poison, "~> 3.0"}
+     {:jason, "~> 1.1"}
     ]
   end
 end
